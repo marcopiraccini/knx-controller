f41bbce10d575854f590541b413994852ab06748
marcopiraccini <marco.piraccini@gmail.com>
2015-10-03 18:09:12 +0200

    Added knx software time/date generator

--- config/logger-config.js
+++ config/logger-config.js
@@ -5,13 +5,15 @@
  */
 var logConfig = {
     name: 'events',
-    streams: [{
-        level: 'debug',
-        type: 'rotating-file',
-        period: '1d',   // daily rotation
-        count: 3,       // keep 3 back copies
-        path: './logs/knx-events.log'
-    },{
+    streams: [
+    //     {
+    //     level: 'debug',
+    //     type: 'rotating-file',
+    //     period: '1d',   // daily rotation
+    //     count: 3,       // keep 3 back copies
+    //     path: './logs/knx-events.log'
+    // },
+    {
         level: 'info',
         stream: process.stdout
     }]
