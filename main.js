f00d0b52499ab5608876742caed6a4e34bf5ce1e
marcopiraccini <marco.piraccini@gmail.com>
2023-01-02 18:59:35 +0100

    upgraded, disabled influx [WIP]

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-var eibd = require('eibd');
-var bunyan = require('bunyan');
-var Hapi = require('hapi');
-var server = new Hapi.Server();
-var fs = require('fs');
-var logConfig = require('./config/logger-config');
-var log = bunyan.createLogger(logConfig);
+const Os = require('os')
+Os.tmpDir = Os.tmpDir || Os.tmpdir
+
+const eibd = require('eibd')
+
+const bunyan = require('bunyan')
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+const fs = require('fs')
+const logConfig = require('./config/logger-config')
+const log = bunyan.createLogger(logConfig)
 
 // TODO: load configurations from config
 // Add the config file position using ENV.
-var knxHost = '192.168.69.172';
-var knxPort = 6720;
-var httpPort = 4000;
+const knxHost = '192.168.69.232'
+const knxPort = 6720
+const httpPort = 4000
 
-var knx = require('./lib/knx');
-var api = require('./lib/api');
-var eventstore = require('./lib/eventstore');
+const knx = require('./lib/knx')
+const api = require('./lib/api')
+// var eventstore = require('./lib/eventstore');
 
 // Init the knx connection and the the othe sub-components
 knx.init(knxHost, knxPort, function (err, knx) {
+  if (err) {
+    console.log('**** Error', err, 'Exiting, check that KNXD is up and running\n')
+    process.exit(1)
+  }
+
+  // Start the event store
+  // eventstore.start(knx)
+
+  // Start the HTTP module (API / Web Content)
+  api.start(httpPort, knx, function (err) {
     if (err) {
-        console.log('**** Error', err, `Exiting, check that KNXD is up and running\n`);
-        process.exit(1);
+      console.log(err)
     }
-
-    // Start the event store
-    eventstore.start(knx);
-
-    // Start the HTTP module (API / Web Content)
-    api.start(httpPort, knx, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        var row = '*********************************************************************';
-        console.log(row);
-        console.log('KNX Controller Service Running on port 4000');
-        console.log(`Connected to KNXD: ${knxHost}, PORT: ${knxPort}`);
-        console.log(row);
-    });
-});
+    const row = '*********************************************************************'
+    console.log(row)
+    console.log('KNX Controller Service Running on port 4000')
+    console.log(`Connected to KNXD: ${knxHost}, PORT: ${knxPort}`)
+    console.log(row)
+  })
+})
